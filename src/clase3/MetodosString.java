5a571a001a7db1b1365a9eeb1e7adfe5719affb4
EleanFP <elean.quintero@alu.ceacfp.es>
2024-10-10 13:16:03 +0200

    Subiendo clase dia 10/10/24

--- src/clase3/MetodosString.java
+++ src/clase3/MetodosString.java
@@ -3,7 +3,22 @@ package clase3;
 public class MetodosString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String txt = holaHola;
+		// txt.startsWith()
+		//txt.endsWith()
+		//txt.substring()
+		//txt.strip()
+		//txt.replace()
+
+		//metodos de string a numero
+		//parseINT()
+		//parseDouble()
+
+		//Metodos por caracter
+		// character.toLowerCase()
+		//Character.isWhitespace()
+		// character.isletter()
 
 	}
 
